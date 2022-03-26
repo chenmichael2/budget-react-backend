2fe8562d7e1b840cc999d6c2eccedc37d64ac162
chenmichael2 <chenmichael2012@gmail.com>
2022-03-25 21:02:49 -0700

    adding app.listen and posts

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const posts = [
+    {
+        username: 'Kyle',
+        title: 'Post 1'
+    },
+    {
+        username: 'Jim',
+        title: 'Post 2'
+    },
+]
+
+app.get('/post', (req, res) => {
+
+})
+
+app.listen(3000);
